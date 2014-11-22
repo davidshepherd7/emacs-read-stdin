d7a3c5bb8a0f8619ff2df2868e6b730ee9a8475f
David Shepherd <davidshepherd7@gmail.com>
2014-11-22 20:53:41 +0000

    Have to use a function for the emacs version to run
    Putting `emacsclient -n -c` doeesn't get the correct quoting.

--- emacs-read-stdin.sh
+++ emacs-read-stdin.sh
@@ -1,22 +1,26 @@
 # include this file in your .bashrc/.zshrc with source "emacs-pipe.sh"
 
+# The emacs or emacsclient to use
+function _emacsfun
+{
+    # Replace with `emacs` to not run as server/client
+    emacsclient -c -n $@
+}
+
+
 # An emacs 'alias' with the ability to read from stdin
 function e
 {
-    # Replace with `emacs` to not run as server/client
-    EMACSBIN='emacsclient -c -n'
-
     # If the argument is - then write stdin to a tempfile and open the
-    # tempfile. Otherwise just run emacsclient. -c and -n are just my
-    # preferred options.
+    # tempfile.
     if [[ $1 == - ]]; then
         tempfile=$(mktemp emacs-stdin-$USER.XXXXXXX --tmpdir)
         cat - > $tempfile
-        $EMACSBIN -e "(progn (find-file \"$tempfile\")
+        _emacsfun -e "(progn (find-file \"$tempfile\")
                              (set-visited-file-name nil)
                              (rename-buffer \"*stdin*\" t))
                  " 2>&1 > /dev/null
     else
-        $EMACSBIN "$@"
+        _emacsfun "$@"
     fi
 }
